2d227f1a813bac4941192d86abe2ccb2f814f08d
yxhwxn <officeyh9819@gmail.com>
2025-03-04 16:39:07 +0900

    Step05: Single Client와 소켓 연결 코드 추가(client에서 메시지를 보내면 서버로부터 수신하는 부분 console로 확인)

--- src/main/java/main/src/step05_socket/SimpleClient.java
+++ src/main/java/main/src/step05_socket/SimpleClient.java
@@ -1,6 +1,9 @@
 package main.src.step05_socket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -14,6 +17,17 @@ public class SimpleClient {
             Socket socket = new Socket(SIMPLE_SERVER_IP, SERVER_PORT);
             System.out.println("서버에 연결되었습니다.");
 
+            try (PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+                String userInput;
+                while ((userInput = stdIn.readLine()) != null) {
+                    out.println(userInput); // 서버로 메시지 전송
+                    System.out.println("서버로부터 수신: " + in.readLine()); // 서버로부터 에코 수신
+                }
+            }
+
         } catch (UnknownHostException e) {
             System.out.println("서버를 찾을 수 없습니다: " + e.getMessage());
         } catch (IOException e) {
@@ -21,4 +35,3 @@ public class SimpleClient {
         }
     }
 }
-
--- src/main/java/main/src/step05_socket/SimpleServer.java
+++ src/main/java/main/src/step05_socket/SimpleServer.java
@@ -1,17 +1,34 @@
 package main.src.step05_socket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * 소켓 기반의 네트워크 프로그램 중 서버 역할을 수행하는 클래스
+ */
 public class SimpleServer {
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         ServerSocket serverSocket = new ServerSocket(5555);
-        System.out.println("서버 소켓 생성, 서버가 시작되었습니다. 클라이언트를 기다립니다.");
+        System.out.println("서버가 시작되었습니다. 클라이언트를 기다립니다.");
 
         Socket socket = serverSocket.accept();
-        System.out.println("클라이언트와 연결되었습니다.");
-    }
+        System.out.println("클라이언트가 연결됨");
 
+        OutputStream out = socket.getOutputStream();
+        InputStream in = socket.getInputStream();
+
+        while (true) {
+            try {
+                int request = in.read();
+                out.write(request);
+            }
+            catch (IOException e) {
+                break;
+            }
+        }
+    }
 }
